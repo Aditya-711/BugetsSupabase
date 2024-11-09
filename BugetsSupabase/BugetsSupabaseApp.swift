79bce42d6117707697b9633705a82b5f19b6e16f
Aditya-711 <64785580+Aditya-711@users.noreply.github.com>
2024-11-09 19:53:21 +0530

    Initial Commit

---
+++ BugetsSupabase/BugetsSupabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugetsSupabaseApp.swift
+//  BugetsSupabase
+//
+//  Created by Aditya Inamdar on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BugetsSupabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
